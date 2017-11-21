0d85929d36223203cdf2b0bb3ef9ed649049ebf7
Nimesh <nimeshp@thoughtworks.com>
2017-11-21 15:34:45 +0530

    removing unwanted print statements method calls

--- android/app/src/main/java/com/wardmap/google/GMapViewManager.java
+++ android/app/src/main/java/com/wardmap/google/GMapViewManager.java
@@ -49,9 +49,6 @@ public class GMapViewManager extends SimpleViewManager<GMapView>
     public void onMapReady(final GoogleMap googleMap) {
         System.out.println("=============== Initialized google maps ===================");
         gMapView.setGoogleMap(googleMap);
-        getDeviceLocation();
-        System.out.println(mLastKnownLocation.getLatitude()+"device cuurent location latitude");
-        System.out.println(mLastKnownLocation.getLongitude()+"device cuurent location longitude");
         googleMap.moveCamera( CameraUpdateFactory.newLatLngZoom(new LatLng(13.082680, 80.270718) , 14.0f) );
         googleMap.getUiSettings().setZoomControlsEnabled(false);
         googleMap.getUiSettings().setScrollGesturesEnabled(false);
