cd398ba04a78b05568e02cf0df833312a718b68a
Nimesh <nimeshp@thoughtworks.com>
2017-11-24 13:35:14 +0530

    Revert "Added splash screen for the app"
    This reverts commit 70b2a12f21ae87ecb3889b738adad11e1d89f075.

--- android/app/src/main/java/com/wardmap/MainActivity.java
+++ android/app/src/main/java/com/wardmap/MainActivity.java
@@ -1,18 +1,8 @@
 package com.wardmap;
 
-import android.os.Bundle;
-
 import com.facebook.react.ReactActivity;
 
-import org.devio.rn.splashscreen.SplashScreen;
-
 public class MainActivity extends ReactActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen.show(this);
-        super.onCreate(savedInstanceState);
-    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
--- android/app/src/main/java/com/wardmap/MainApplication.java
+++ android/app/src/main/java/com/wardmap/MainApplication.java
@@ -3,7 +3,6 @@ package com.wardmap;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-  import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import io.realm.react.RealmReactPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -27,7 +26,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new SplashScreenReactPackage(),
             new RealmReactPackage(),
               new MapsPackage(),
                 new ReactComponentPackage()
